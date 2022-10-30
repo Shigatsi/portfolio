5d22eb9d6205d4898d75c85412500c186beae9d8
Shigatsi Shigatsi <shigatsii@gmail.com>
2022-10-31 00:07:14 +0300

    feat: sanity about added

--- src/container/About/About.tsx
+++ src/container/About/About.tsx
@@ -1,40 +1,35 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-import { images } from '../../utils/constants';
+import { urlFor, client } from "../../client";
 import "./About.scss";
 
-const abouts = [
-  {
-    title: "Frontend Development",
-    description: "I am a good web-developer",
-    img: images.about01,
-  },
-  {
-    title: "Back-end Development",
-    description: "I am a good back-developer",
-    img: images.about02
-  },
-  {
-    title: "MERN Stack",
-    description: "I am a good back-developer",
-    img: images.about02
-  },
-  {
-    title: "MERN Stack",
-    description: "I am a good back-developer",
-    img: images.about02
-  },
-];
+interface IAbouts {
+  title: string;
+  description: string;
+  img:string;
+}
 
 const About: React.FC = () => {
+  const [abouts, setAbouts] = React.useState<IAbouts[]>([]);
+
+  React.useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
+
   return (
     <>
       <h2 className="about__header head-text">
-        I Know that&ensp;<span>Good Development</span><br />Means&ensp;<span>Good Apps</span>
+        I Know that&ensp;<span>Good Development</span>
+        <br />
+        Means&ensp;<span>Good Apps</span>
       </h2>
       <div className="about__profiles">
         {abouts.map((about, index) => (
+          <>
+          {console.log('urlFor(about.img)', urlFor(about.img).options.baseUrl)}
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
@@ -42,10 +37,14 @@ const About: React.FC = () => {
             className="about__profile-item"
             key={about.title + index}
           >
-            <img src={about.img} alt={about.title} />
+            
+            <img src={urlFor(about.img).options.baseUrl} alt={about.title} />
             <h2 className="about__profile-item-title">{about.title}</h2>
-            <p className="about__profile-item-text p-text">{about.description}</p>
+            <p className="about__profile-item-text p-text">
+              {about.description}
+            </p>
           </motion.div>
+          </>
         ))}
       </div>
     </>
